5141e5ab1f0a449db0f4c22dc04b247c8f1f8810
ippsav <mehdiboujid@gmail.com>
2024-03-13 00:31:10 +0000

    implement decoding a bencode list

--- app/bencoded.zig
+++ app/bencoded.zig
@@ -5,17 +5,45 @@ const ParseResult = struct { value: Value, new_cursor: usize };
 pub const Value = union(enum) {
     string: []const u8,
     int: i64,
+    list: []Value,
 
-    fn parseValue(bytes: []const u8, cursor: usize) !ParseResult {
-        if (bytes[cursor] == 'i') {
-            return parseAssignedInt(bytes, cursor);
-        } else if (std.mem.indexOfScalar(u8, bytes, ':')) |i| {
-            return parseString(bytes, cursor, i);
+    fn parseValue(gpa: std.mem.Allocator, bytes: []const u8, cursor: usize) !ParseResult {
+        switch (bytes[cursor]) {
+            '0'...'9' => {
+                return parseString(bytes, cursor);
+            },
+            'i' => return parseAssignedInt(bytes, cursor),
+            'l' => return parseList(gpa, bytes, cursor),
+            else => return error.InvalidToken,
         }
-        return error.InvalidToken;
     }
 
-    fn parseString(bytes: []const u8, cursor: usize, delimiter_index: usize) ParseResult {
+    fn parseList(allocator: std.mem.Allocator, bytes: []const u8, cursor: usize) ParseResult {
+        var index = cursor + 1;
+        var values_list = std.ArrayList(Value).init(allocator);
+        defer values_list.deinit();
+        while (bytes[index] != 'e' and index != bytes.len - 1) {
+            const parse_result = Value.parseValue(allocator, bytes, index) catch {
+                fatal("Error parsing list", .{});
+            };
+            values_list.append(parse_result.value) catch {
+                fatal("Out of memory, exiting...", .{});
+            };
+            index = parse_result.new_cursor;
+        }
+        const list = values_list.toOwnedSlice() catch {
+            fatal("Boom", .{});
+        };
+        return .{ .value = .{ .list = list }, .new_cursor = index };
+    }
+
+    fn parseString(bytes: []const u8, cursor: usize) ParseResult {
+        var delimiter_index: usize = undefined;
+        if (std.mem.indexOfScalar(u8, bytes[cursor..], ':')) |v| {
+            delimiter_index = v + cursor;
+        } else {
+            fatal("Error parsing string missing delimiter ':'", .{});
+        }
         const string_len = std.fmt.parseInt(usize, bytes[cursor..delimiter_index], 10) catch {
             fatal("Error parsing string length", .{});
         };
@@ -24,13 +52,16 @@ pub const Value = union(enum) {
     }
 
     fn parseAssignedInt(bytes: []const u8, cursor: usize) ParseResult {
-        const number_length = std.mem.indexOfScalar(u8, bytes[cursor..], 'e') orelse {
-            fatal("Error parsing integer, missing end (e)", .{});
+        const number_length = blk: {
+            const e_tag_index = std.mem.indexOfScalar(u8, bytes[cursor..], 'e') orelse {
+                break :blk fatal("Error parsing integer, missing end (e)", .{});
+            };
+            break :blk e_tag_index - 1;
         };
-        const parsedNumber = std.fmt.parseInt(i64, bytes[cursor + 1 .. number_length], 10) catch {
-            fatal("Error parsing integer: {s}", .{bytes[cursor + 1 .. number_length]});
+        const parsedNumber = std.fmt.parseInt(i64, bytes[cursor + 1 .. cursor + 1 + number_length], 10) catch {
+            fatal("Error parsing integer: {s}", .{bytes[cursor + 1 .. cursor + 1 + number_length]});
         };
-        return .{ .value = Value{ .int = parsedNumber }, .new_cursor = cursor + number_length + 1 };
+        return .{ .value = Value{ .int = parsedNumber }, .new_cursor = cursor + number_length + 2 };
     }
 
     pub fn toJsonValue(self: Value, allocator: std.mem.Allocator) ![]const u8 {
@@ -45,6 +76,20 @@ pub const Value = union(enum) {
                 var string = std.ArrayList(u8).init(allocator);
                 defer string.deinit();
                 try std.json.stringify(n, .{}, string.writer());
+                return string.toOwnedSlice();
+            },
+            .list => |l| {
+                var string = std.ArrayList(u8).init(allocator);
+                defer string.deinit();
+                var writer = string.writer();
+                _ = try writer.write("[");
+                for (l, 0..) |v, i| {
+                    const json_value = try v.toJsonValue(allocator);
+                    _ = try writer.write(json_value);
+                    allocator.free(json_value);
+                    if (i != l.len - 1) try writer.writeByte(',');
+                }
+                _ = try writer.write("]");
                 return string.toOwnedSlice();
             },
         }
@@ -65,10 +110,10 @@ pub const Scanner = struct {
     }
 
     pub fn next(self: *Scanner) ?Value {
-        if (self.cursor == self.input.len) {
+        if (self.cursor == self.input.len - 1) {
             return null;
         }
-        const parse_result = Value.parseValue(self.input, self.cursor) catch {
+        const parse_result = Value.parseValue(self.gpa, self.input, self.cursor) catch {
             fatal("error parsing at {d}\n", .{self.cursor});
         };
         self.cursor = parse_result.new_cursor;
