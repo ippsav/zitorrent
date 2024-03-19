e7886494567f1b8eb661fec1f06696714f1baffc
ippsav <mehdiboujid@gmail.com>
2024-03-19 01:26:40 +0000

    fix formatting

--- app/main.zig
+++ app/main.zig
@@ -45,13 +45,22 @@ pub fn main() !void {
             const torrent_meta_data = try TorrentMetadata.getTorrentMetadata(decoded_content);
             const hash = try torrent_meta_data.info.getInfoHash();
             const piece_hashes = torrent_meta_data.info.getPiecesHashes();
+            // Tracker URL: http://bittorrent-test-tracker.codecrafters.io/announce
+            // Length: 92063
+            // Info Hash: d69f91e6b2ae4c542468d1073a71d4ea13879a7f
+            // Piece Length: 32768
+            // Piece Hashes:
+            // e876f67a2a8886e8f36b136726c30fa29703022d
+            // 6e2275e604a0766656736e81ff10b55204ad8d35
+            // f00d937a0213df1982bc8d097227ad9e909acc17
             try stdout.print(
                 \\Tracker URL: {s}
                 \\Length: {d}
                 \\Info Hash: {s}
-                \\Pieces:
+                \\Piece Length: {d}
+                \\Pieces Hashes:
                 \\
-            , .{ torrent_meta_data.announce, torrent_meta_data.info.length, std.fmt.fmtSliceHexLower(&hash) });
+            , .{ torrent_meta_data.announce, torrent_meta_data.info.length, std.fmt.fmtSliceHexLower(&hash), torrent_meta_data.info.@"piece length" });
             for (piece_hashes) |h| {
                 try stdout.print("{s}\n", .{std.fmt.fmtSliceHexLower(&h)});
             }
